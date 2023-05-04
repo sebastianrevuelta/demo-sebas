089ca25205cdd8225d1426a0dd2e451a3767c4ed
Sebastian Revuelta <sebastianrevuelta@gmail.com>
2023-05-04 13:06:13 +0200

    Delete pytest2.py

--- pytest2.py
+++
@@ -1,9 +0,0 @@
-from urllib.request import URLopener
-
-def test1():
-    od = URLopener()
-    # ruleid: insecure-urlopener-open-ftp
-    od.open("ftp://example.com")
-
-def test1_ok():
-    od = URLopener()
